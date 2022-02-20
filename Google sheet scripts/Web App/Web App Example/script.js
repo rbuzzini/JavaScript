170dcff203607d72c5fc49af2385f4633b180ff8
Roberto Buzzini <roberto.buzzini95@gmail.com>
2022-02-20 17:44:20 +0100

    Basic web app updated

--- Google sheet scripts/Web App/Web App Example/script.js
+++ Google sheet scripts/Web App/Web App Example/script.js
@@ -1,21 +1,24 @@
 function doGet(e) {
   
-    Logger.log(e.parameter);
-    return HtmlService.createHtmlOutputFromFile("page");
+    return HtmlService.createTemplateFromFile("page").evaluate();
     
   }
   
   
-  function userClicked(name){
+  function userClicked(userInfo){
       var id = "17ZxneKmJWmLpIj_byJBJcfor8Iagak8sENQBgmtKT9c";   // spreadsheet id where we wanna save the data
       var ss = SpreadsheetApp.openById(id);    // ss stands for spreadsheet
       var ws = ss.getSheetByName("Dati");   // ws stands for worksheet
       
-      ws.appendRow([name, new Date()]);
+      ws.appendRow([userInfo.firstName, userInfo.lastName, userInfo.app, new Date()]);
       
-      Logger.log(name + " clicked the button");
+      Logger.log(fname + " clicked the button");
   }
   
+  
+  function include(fileName){
+    return HtmlService.createHtmlOutputFromFile(fileName).getContent();   // function used in page.html file to include style and js text
+  }
   
   // References: https://www.youtube.com/watch?v=RRQvySxaCW0&list=PLv9Pf9aNgemt82hBENyneRyHnD-zORB3l&index=1
   
