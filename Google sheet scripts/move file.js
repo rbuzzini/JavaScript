d556a49c6bd7184bded1f2be0aa231fff55cc805
Roberto Buzzini <roberto.buzzini95@gmail.com>
2022-06-05 17:43:35 +0200

    Modified google script tip

--- Google sheet scripts/move file.js
+++ Google sheet scripts/move file.js
@@ -1,95 +1,112 @@
-function moving_file(){
+// This script works if you want to move a file into a sub folder inside
+// a master destination folder. The sub folder in which you want to move the
+// file changes based on specific column values in your data.
+
+
+const master_destination_folder_id = ''
+const method1_folder_id = ''
+const method2_folder_id = ''
+
+
+const ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Insert sheet name')
+const ss_data = ss.getRange('A1').getDataRegion().getValues()
+
+
+function move_file() {
+
+
+  var branch_column_name = ''
+  var sales_column_name = ''
+  var method_final_column_name = ''
+  var link_column_name = ''
+  var check_col_name = ''
+
+
+  var branch_column_index = ss_data[0].indexOf(branch_column_name)
+  var sales_column_index = ss_data[0].indexOf(sales_column_name)
+  var method_final_column_index = ss_data[0].indexOf(method_final_column_name)
+  var link_column_index = ss_data[0].indexOf(link_column_name)
+  var check_column_index = ss_data[0].indexOf(check_col_name)
+
+
+  // Creo le liste con i valori:
+  //var lista_branch = []
+  var lista_sales = []
+  var lista_method_final = []
+  var lista_link = []
+  var lista_check = []
   
-    // parameter setting:
   
-    // folders ids:
-    var idFolderByLetter = '18fYgk6t2uCAiOkiVPRi3EHCpvA5Og9Nh'
-    var idFolderBySales = '1qu8tpfgd07siS8J8Xgf0vOFZZ_QRCl1p'
+
+  ss_data.map(
+    (row, i) => {
+      //lista_branch.push(row[branch_column_index]);
+      lista_sales.push(row[sales_column_index]);
+      lista_method_final.push(row[method_final_column_index]);
+      lista_link.push(row[link_column_index]);
+      lista_check.push(row[check_column_index]);
+      }   
+  )
+
+
+  // Ora bisogna iterare nelle liste e passare quando lista_check=true, quando lista_check=false ...
   
-  
-  
-    // data preparation:
-    var ss = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Generated Letters')
-    var ssData = ss.getRange('A1').getDataRegion().getValues()
-  
-    // columns names:
-    checkColumnName = 'Check'
-    methodColumnName = 'Method'
-    methodFinalColumnName = 'Method Final'
-    salesNameColumnName = 'Sale Rep'
-    linkColumnName = 'idPDF'
-  
-    // columns indexes:
-    checkColumnIndex = ssData[0].indexOf(checkColumnName)
-    methodColumnIndex = ssData[0].indexOf(methodColumnName)
-    methodFinalColumnIndex = ssData[0].indexOf(methodFinalColumnName)
-    salesNameColumnIndex = ssData[0].indexOf(salesNameColumnName)
-    linkColumnIndex = ssData[0].indexOf(linkColumnName)
-  
-  
-  
-    // Create lists with data values:
-    var lista_check = []
-    var lista_method = []
-    var lista_methodFinal = []
-    var lista_salesName = []
-    var lista_link = []
+
+  ss_data.map(
+    (el, index) => {
+
     
-    ssData.map(
-      (row, i) => {
-        lista_check.push(row[checkColumnIndex]);
-        lista_method.push(row[methodColumnIndex]);
-        lista_methodFinal.push(row[methodFinalColumnIndex]);
-        lista_salesName.push(row[salesNameColumnIndex]);
-        lista_link.push(row[linkColumnIndex]);
-        }   
-    )  // end map - lists completed
-  
-  
-    // Now you need to iterate over the lists and pass when lista_check=true; when lista_check=false ...
-  
-    lista_check.map(
-      (el, index) => {
-        if(el==false) {
-            if(lista_method[index]=='by Letter') {
-              var fileToMove = DriveApp.getFileById(lista_link[index]);
-              var destinationFolder = getDestinationFolder(idFolderBySales, lista_salesName[index]);
-              fileToMove.moveTo(destinationFolder);
-            } else if(lista_method[index]=='by Sales') {
-                var fileToMove = DriveApp.getFileById(lista_link[index]);
-                var destinationFolder = DriveApp.getFolderById(idFolderByLetter);
-                fileToMove.moveTo(destinationFolder);
-            }
-        }
+      if (index != 0 && lista_check[index] == false) {
+      var file_to_copy = DriveApp.getFileById(lista_link[index]);
+      var temp_method_final = lista_method_final[index];
+      var target_folder = "";
+
+      if(temp_method_final == "method1"){
+        target_folder = getDestinationFolder(method1_folder_id, lista_sales[index]);
+        
+      } else if(temp_method_final == "method2"){
+        target_folder = DriveApp.getFolderById(method2_folder_id);
       }
-    )   // end map
-  
-  };   // end function
-  
-  
-  
-  
-  /////////////////////////////////////////   Support   ////////////////////////////////////////////
-  
-  
-  // support function to find the folder to search:
-  function getDestinationFolder(idParentFolder, folderNameToSearch){
-    
-    var parentFolder = DriveApp.getFolderById(idParentFolder);
-    var foldersList = parentFolder.getFolders();
-  
-    while(foldersList.hasNext()) {
-      var nextFolder = foldersList.next();
-      if(nextFolder.getName()==folderNameToSearch) {
-        return nextFolder;
+
+      file_to_copy.moveTo(target_folder);
+      
       }
+       
     }
-  
-  }; // end function
-  
-  
-  
-  
-  
-  
-  
+    
+  )
+
+};
+
+
+
+
+
+
+
+/////////////////////////////////////////   Support   ////////////////////////////////////////////
+
+
+// Support function to find the folder to search:
+function getDestinationFolder(idParentFolder, folderNameToSearch){
+
+  var parentFolder = DriveApp.getFolderById(idParentFolder);
+  var foldersList = parentFolder.getFolders();
+  var flagCreation = false;
+
+  while(foldersList.hasNext()) {
+    var nextFolder = foldersList.next();
+    if(nextFolder.getName()==folderNameToSearch) {
+      flagCreation = true;
+      return nextFolder
+    }
+  }
+
+  // if the folder we are searching for doesn't exist, we create it
+  if(flagCreation == false){
+    var newFolder = parentFolder.createFolder(folderNameToSearch)
+    return DriveApp.getFolderById(newFolder.getId())
+  }
+
+
+};
